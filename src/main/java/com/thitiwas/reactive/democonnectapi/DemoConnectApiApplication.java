3a3004eb2b601c40e85aef59e7fbcb4a8e291578
Thitiwas <thitiwas.nn@gmail.com>
2023-08-14 00:36:30 +0700

    first

---
+++ src/main/java/com/thitiwas/reactive/democonnectapi/DemoConnectApiApplication.java
@@ -0,0 +1,13 @@
+package com.thitiwas.reactive.democonnectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConnectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConnectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thitiwas/reactive/democonnectapi/DemoConnectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thitiwas.reactive.democonnectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConnectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
